56322b33af6ba98c02cede904c5e8b6a18d07105
SUKJIN LEE <45951105+stonetruth@users.noreply.github.com>
2026-09-22 12:35:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const app = express()
 
+const server = app.listen(8080, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host,port);
+    })
+
+app.get('/get/information',function(req,res){
+    console.log("get request received from client");
+    res.send("success");
+    })
